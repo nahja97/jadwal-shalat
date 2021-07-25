2d3507f1337769232c230ea201e834ef92f81cec
nahja falahil umam <nahjafalahilumam@gmail.com>
2021-07-26 02:52:04 +0700

    add geolocation feature

--- apps/jadwal-shalat/pages/index.tsx
+++ apps/jadwal-shalat/pages/index.tsx
@@ -6,19 +6,19 @@ interface Props {
   layout?: string;
 }
 
-function Index() {
+function Index({mapToken}) {
   const [city, setCity] = useState()
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async function(position) {
-        let res = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=AIzaSyCXZ3-luH5PdTcsAsilZR9n76RjE6mIRqY')
+        let res = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ',' + position.coords.longitude + '&key=' + mapToken)
         let data = await res.json()
         data = data.results[0]?.address_components[5]?.long_name
         setCity(data ? (data.split(' '))[1] : 'Malang')
       });
     }
 
-    
+
   })
 
   return <>
@@ -28,9 +28,11 @@ function Index() {
 
 export async function getStaticProps() {
   const layout = 'default'
+  const mapToken = process.env.MAP_TOKEN
   return {
     props: {
-      layout
+      layout,
+      mapToken
     },
   }
 }
