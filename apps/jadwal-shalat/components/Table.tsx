e6f9efbaaf0db1a8b595e7b5dcc35aab5c626c5c
nahja falahil umam <nahjafalahilumam@gmail.com>
2021-07-27 03:37:11 +0700

    fix Table Component

--- apps/jadwal-shalat/components/Table.tsx
+++ apps/jadwal-shalat/components/Table.tsx
@@ -6,42 +6,44 @@ function Table({schedule}) {
     return (
         <div className={styles.table_container}>
             <table>
-                <tr>
-                    <th>Tengah Malam</th>
-                    <td>{schedule['Midnight']}</td>
-                </tr>
-                <tr>
-                    <th>Imsak</th>
-                    <td>{schedule['Imsak']}</td>
-                </tr>
-                <tr>
-                    <th>Fajar</th>
-                    <td>{schedule['Fajr']}</td>
-                </tr>
-                <tr>
-                    <th>Terbit Matahari</th>
-                    <td>{schedule['Sunrise']}</td>
-                </tr>
-                <tr>
-                    <th>Dzuhur</th>
-                    <td>{schedule['Dhuhr']}</td>
-                </tr>
-                <tr>
-                    <th>Ashar</th>
-                    <td>{schedule['Asr']}</td>
-                </tr>
-                <tr>
-                    <th>Tenggelam Matahari</th>
-                    <td>{schedule['Sunset']}</td>
-                </tr>
-                <tr>
-                    <th>Maghrib</th>
-                    <td>{schedule['Maghrib']}</td>
-                </tr>
-                <tr>
-                    <th>Isya'</th>
-                    <td>{schedule['Isha']}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <th>Tengah Malam</th>
+                        <td>{schedule['Midnight']}</td>
+                    </tr>
+                    <tr>
+                        <th>Imsak</th>
+                        <td>{schedule['Imsak']}</td>
+                    </tr>
+                    <tr>
+                        <th>Fajar</th>
+                        <td>{schedule['Fajr']}</td>
+                    </tr>
+                    <tr>
+                        <th>Terbit Matahari</th>
+                        <td>{schedule['Sunrise']}</td>
+                    </tr>
+                    <tr>
+                        <th>Dzuhur</th>
+                        <td>{schedule['Dhuhr']}</td>
+                    </tr>
+                    <tr>
+                        <th>Ashar</th>
+                        <td>{schedule['Asr']}</td>
+                    </tr>
+                    <tr>
+                        <th>Tenggelam Matahari</th>
+                        <td>{schedule['Sunset']}</td>
+                    </tr>
+                    <tr>
+                        <th>Maghrib</th>
+                        <td>{schedule['Maghrib']}</td>
+                    </tr>
+                    <tr>
+                        <th>Isya'</th>
+                        <td>{schedule['Isha']}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
