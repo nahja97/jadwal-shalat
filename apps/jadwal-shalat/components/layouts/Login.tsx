e86b3596c594b73a08f3fcc6aeca12af26e5e9ea
nahja falahil umam <nahjafalahilumam@gmail.com>
2021-07-27 19:18:22 +0700

    Add initial Login Page

--- apps/jadwal-shalat/components/layouts/Login.tsx
+++ apps/jadwal-shalat/components/layouts/Login.tsx
@@ -1,8 +1,13 @@
 import React, { Children, FC } from "react"
+import styles from '../../styles/LoginLayout.module.css';
 
 const Login: FC<{}> = ({ children }) => {
     return <>
-      {children}
+      <main className={styles.login_page}>
+        <div className={styles.container}>
+         {children}
+        </div>
+      </main>
     </>
   }
 
